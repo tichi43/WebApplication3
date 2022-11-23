5172a0775994f805de934b731cd214ec1649b170
tichi43 <tichi43@gmail.com>
2022-11-23 13:16:04 +0100

    Add project files.

---
+++ WebApplication3/wwwroot/geek.js
@@ -0,0 +1,25 @@
+﻿var viccek;
+
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < d.length; i++) {
+        //console.log(d[i].question);
+        let elem = document.createElement("li")
+        elem.innerHTML = d[i].text;
+        document.getElementById("ide").appendChild(elem);
+    }
+
+}
+
+window.onload = function () {
+    fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+);
+
+
+};
---
+++ WebApplication3/wwwroot/jsgyak.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+
+    function factorialize(num) {
+        if (num < 0)
+            return -1;
+        else if (num == 0)
+            return 1;
+        else {
+            return (num * factorialize(num - 1));
+        }
+    }
+    console.log(factorialize(3));
+
+   // document.getElementById("container").style.width = 10 * 30;
+
+    for (var sor = 0; sor < 10; sor++) {
+        var újsor = document.createElement("div");
+        újsor.classList.add("sor");
+        document.getElementById("container").appendChild(újsor);
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var újelem = document.createElement("div");
+            újelem.classList.add("elem");
+            let szám = factorialize(sor) / (factorialize(oszlop) * factorialize(sor - oszlop));
+            újelem.innerHTML = szám;
+            újelem.style.backgroundColor = `rgba(${szám},${155-szám},${60-szám})`
+            újsor.appendChild(újelem);
+        }
+    }
+
+
+}
