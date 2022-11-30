7839f84e0e0191f131850e8a7d4a5305fc114d10
tichi43 <tichi43@gmail.com>
2022-11-30 13:11:28 +0100

    harmadik feléig

---
+++ WebApplication3/wwwroot/hajos.js
@@ -0,0 +1,15 @@
+﻿async function start() {
+    const response = await fetch("/api/Hajos/questions/all");
+    const data = await response.json();
+    kiíratás(data);
+}
+
+function kiíratás(kérdések) {
+    console.log(kérdések);
+    kérdések.forEach(function (item) {
+        let újelem = document.createElement("div");
+        újelem.innerHTML = (item);
+        document.getElementById("eredmeny").appendChild(újelem);
+    });
+}
+start();
