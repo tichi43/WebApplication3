2e24a97da39338f264ac19edec9c99028b959842
tichi43 <tichi43@gmail.com>
2022-11-30 12:19:47 +0100

    második feladat készelső

---
+++ WebApplication3/Controllers/HajosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HajosController : ControllerBase
+    {
+        Models.HajosContext hajosdb = new Models.HajosContext();
+
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult r1()
+        {
+            var kérdések = from x in hajosdb.Questions select x.Question1;
+
+            return new JsonResult(kérdések);
+        }
+
+
+    }
+}
--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,8 +17,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//app.UseAuthorization();
 
 app.MapControllers();
-app.UseStaticFiles();
+
 app.Run();
